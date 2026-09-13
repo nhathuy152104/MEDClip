abf64015405099c806ba5ad83c2c19ef349c6d09
arsene_lupin <arsene_lupin@trunghieubh0102@gmail.com>
2026-09-13 12:02:06 +0700

    new ver

--- medclip/dataset.py
+++ medclip/dataset.py
@@ -136,6 +136,8 @@ class MedCLIPFeatureExtractor(CLIPFeatureExtractor):
         '''
         x, y = img.size
         print(x,y)
+        print(min_size)
+        print(type(min_size))
         size = max(min_size, x, y)
         new_im = Image.new('L', (size, size), fill_color)
         new_im.paste(img, (int((size - x) / 2), int((size - y) / 2)))
